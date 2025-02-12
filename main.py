93f6f8a23687452cb04549ca89d86934989d6a20
stanislav <stanislav.vasilchenko@yandex.ru>
2025-02-12 19:36:34 +0300

    [+] Роутер пользователей

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+from sys import prefix
+
 import uvicorn
 from fastapi import FastAPI
+from src.routers.users import router as users_router
 
-app = FastAPI()
+app = FastAPI(prefix="api/")
+app.include_router(users_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
