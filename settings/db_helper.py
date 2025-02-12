669a362e60926f758ec011b67bfa0f338c8fce26
stanislav <stanislav.vasilchenko@yandex.ru>
2025-02-12 19:34:30 +0300

    [+] Добавлен функционал подключения к БД для репозитория

--- settings/db_helper.py
+++ settings/db_helper.py
@@ -1,19 +1,46 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import (
+    create_async_engine,
+    async_sessionmaker,
+    async_scoped_session,
+    AsyncSession,
+)
+from asyncio import current_task
 
 from settings.config import settings
 
 
 class DatabaseHelper:
-    def __init__(self, url:str, echo:bool=False):
-        self.enhine = create_async_engine(
-            url = url,
-            echo = echo,
+    def __init__(self, url: str, echo: bool = False):
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
         )
         self.session_factory = async_sessionmaker(
-            bind = self.enhine,
-            autocommit = False,
-            autoflush = False,
-            expire_on_commit = False,
+            bind=self.engine,
+            autocommit=False,
+            autoflush=False,
+            expire_on_commit=False,
         )
 
-db_helper = DatabaseHelper(url=settings.db_url, echo=settings.db_echo)
+    def scoped_session(self):
+        session = async_scoped_session(
+            self.session_factory,
+            scopefunc=current_task,
+        )
+        return session
+
+    async def session_dependency(self) -> AsyncSession:
+        async with self.session_factory() as session:
+            yield session
+            await session.close()
+
+    async def scoped_session_dependency(self) -> AsyncSession:
+        session = self.scoped_session()
+        yield session
+        await session.close()
+
+
+db_helper = DatabaseHelper(
+    url=settings.db_url,
+    echo=settings.db_echo,
+)
