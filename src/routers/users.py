6f6c50473c032f70d2448dbc89c1d403554abe02
stanislav <stanislav.vasilchenko@yandex.ru>
2025-02-12 19:36:17 +0300

    [+] Роут для создания пользователя

---
+++ src/routers/users.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Depends
+from src.schemes.users import UserOut, UserCreate
+from src.services.users import UserService
+
+service = UserService()
+router = APIRouter(tags=["users"])
+
+
+@router.post("/", response_model=UserOut)
+async def create_user(user: UserCreate):
+    return await service.create_user(user)
