4279b40f704318d7b3d47066ce3a49b128e9a3a8
stanislav <stanislav.vasilchenko@yandex.ru>
2025-02-13 15:37:22 +0300

    [*] black

--- settings/config.py
+++ settings/config.py
@@ -6,12 +6,14 @@ load_dotenv()
 
 
 class Settings(BaseSettings):
-    db_url: str = (f"postgresql+asyncpg://"
-                   f"{getenv("DB_USER")}:"
-                   f"{getenv("DB_PASSWORD")}@"
-                   f"{getenv("DB_HOST")}:"
-                   f"{getenv("DB_PORT")}/"
-                   f"{getenv("DB_NAME")}")
+    db_url: str = (
+        f"postgresql+asyncpg://"
+        f"{getenv("DB_USER")}:"
+        f"{getenv("DB_PASSWORD")}@"
+        f"{getenv("DB_HOST")}:"
+        f"{getenv("DB_PORT")}/"
+        f"{getenv("DB_NAME")}"
+    )
     db_echo: bool = False
 
 
