edd3918e7d6e71eaae92a25eb0f3a75db61aba6c
stanislav <stanislav.vasilchenko@yandex.ru>
2025-02-14 16:21:23 +0300

    [+] Добавлен роут для реф кода

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from sys import prefix
 import uvicorn
 from fastapi import FastAPI
 from src.routers.users import router as users_router
+from src.routers.referral_code import router as ref_code_router
 
 app = FastAPI(prefix="api/")
 app.include_router(users_router)
+app.include_router(ref_code_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
