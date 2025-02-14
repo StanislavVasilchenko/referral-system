762fa85a07d7aad3862e4a7c8a96df4c8de425a5
stanislav <stanislav.vasilchenko@yandex.ru>
2025-02-14 12:08:22 +0300

    [*] Изменен функционал настроек

--- settings/config.py
+++ settings/config.py
@@ -1,11 +1,15 @@
 from os import getenv
+from pathlib import Path
+
 from dotenv import load_dotenv
 from pydantic_settings import BaseSettings
 
 load_dotenv()
 
+BASE_DIR = Path(__file__).parent.parent
 
-class Settings(BaseSettings):
+
+class DatabaseSettings(BaseSettings):
     db_url: str = (
         f"postgresql+asyncpg://"
         f"{getenv("DB_USER")}:"
@@ -15,6 +19,20 @@ class Settings(BaseSettings):
         f"{getenv("DB_NAME")}"
     )
     db_echo: bool = False
+
+
+class AuthSettings(BaseSettings):
+    private_key: Path = BASE_DIR / "certs" / "private.pem"
+    public_key: Path = BASE_DIR / "certs" / "public.pem"
+    algorithm: str = "RS256"
+    access_token_expire: int = 3
+
+
+class Settings(BaseSettings):
+    # database
+    db: DatabaseSettings = DatabaseSettings()
+    # jwt_auth
+    jwt_auth: AuthSettings = AuthSettings()
 
 
 settings = Settings()
