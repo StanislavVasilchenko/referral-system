23fa169d67a579e2cb429ab6389ac48f2d0afae1
stanislav <stanislav.vasilchenko@yandex.ru>
2025-02-15 13:05:43 +0300

    [+] Добавлены поля для рефералов

--- src/models/user.py
+++ src/models/user.py
@@ -1,4 +1,6 @@
 from typing import TYPE_CHECKING
+
+from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from settings.base import Base
 
@@ -9,8 +11,20 @@ if TYPE_CHECKING:
 class User(Base):
     email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[bytes]
+    referred_by: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=True)
 
-    referral_codes: Mapped[list["ReferralCode"]] = relationship(back_populates="user")
+    referral_codes: Mapped[list["ReferralCode"]] = relationship(
+        back_populates="user",
+    )
+    referrals: Mapped[list["User"]] = relationship(
+        "User",
+        back_populates="referrer",
+    )
+    referrer: Mapped["User"] = relationship(
+        "User",
+        remote_side="User.id",
+        back_populates="referrals",
+    )
 
     def __str__(self):
         return self.email
